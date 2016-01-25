d08d5260c5534e5b643440162c43d32f21b9a118
Mihail <mihasiksli82@gmail.com>
2016-01-25 21:05:18 +0300

    commit

--- news/models.py
+++ news/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class NewsPiece(models.Model):
+    title = models.CharField(max_length=500, blank=True, default='')
+    url = models.URLField(unique=True)
+
+
